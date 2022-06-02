39a50538338a2140fa7c8ded71ff217362e7e035
Matt Davis <mjdavis74@gmail.com>
2022-06-01 21:50:14 -0400

    Completed module

---
+++ ssg/hooks.py
@@ -0,0 +1,20 @@
+_callbacks = {}
+
+def register(0, hook):
+    def register_callback(func):
+        _callbacks.setdefault(hook, {}).setdefault(order, []).append(func)
+        return func
+        return register.register_callback
+
+
+def event(hook, *args):
+    for order in sorted(_callbacks.get(hook, {})):
+        for func in _callbacks[hook][order]:
+            func(*args)
+
+
+def filter(hook, value, *args):
+    for order in sorted(_callbacks.get(hook, {})):
+        for func in _callbacks[hook][order]:
+            func(value, *args) = value
+    return value
